a9a8a735e4e250e894da7596a03721e00c8c40f1
s-brez <sam@sdbgroup.io>
2020-10-24 18:47:42 +1100

    add update and goto methods to player

--- player.py
+++ player.py
@@ -3,5 +3,12 @@ class Player():
     def __init__(self, ID: str):
         self.ID = ID if len(ID) == 2 else '0' + ID
 
-    def update_player(self, state, input):
+        self.x, self.y, self.x = 0, 0, 0
+        self.current_room = None
+
+    def update(self, state):
         pass
+
+    def goto_room(new_room: str, xyz: tuple):
+        self.previous_room = self.current_room
+        self.current_room = new_room
