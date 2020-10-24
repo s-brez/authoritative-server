6185351a1c361eab3838691cfcabc9f63e7d08db
s-brez <sam@sdbgroup.io>
2020-10-24 20:23:41 +1100

    add client message deserialization for client messages

--- messaging.py
+++ messaging.py
@@ -19,21 +19,26 @@ CLIENT_INPUT = {
         '68': 0,    # d
         'Key.space': 0,
 
+        # use/interact
+        '69': 0,   # e
+
         # skills 1-12
-        '49': 0,
-        '50': 0,
-        '51': 0,
-        '52': 0,
-        '53': 0,
-        '54': 0,
-        '55': 0,
-        '56': 0,
-        '57': 0,
-        '48': 0,
-        '189': 0,
-        '187': 0,
+        '49': 0,    # 1
+        '50': 0,    # 2
+        '51': 0,    # 3
+        '52': 0,    # 4
+        '53': 0,    # 5
+        '54': 0,    # 6
+        '55': 0,    # 7
+        '56': 0,    # 8
+        '57': 0,    # 9
+        '48': 0,    # 0
+        '189': 0,   # -
+        '187': 0,   # =
 
         # client-only actions
+
+        # misc
         'Key.esc': 0,
 
         # modifiers
@@ -52,31 +57,75 @@ CLIENT_INPUT = {
     },
 }
 
+INPUT_ITEMS = [
+        'up',
+        'left',
+        'down',
+        'right',
+        'jump',
+        'use',
+        's1',
+        's2',
+        's3',
+        's4',
+        's5',
+        's6',
+        's7',
+        's8',
+        's9',
+        's10',
+        's11',
+        's12']
 
-def serialize_client_input(player_input):
 
+def serialize_client_input(input):
     return (
-        (1 << 0 if player_input['up'] else 0) +
-        (1 << 1 if player_input['left'] else 0) +
-        (1 << 2 if player_input['down'] else 0) +
-        (1 << 3 if player_input['right'] else 0) +
-        (1 << 4 if player_input['jump'] else 0) +
-        (1 << 5 if player_input['s1'] else 0) +
-        (1 << 6 if player_input['s2'] else 0) +
-        (1 << 7 if player_input['s3'] else 0) +
-        (1 << 8 if player_input['s4'] else 0) +
-        (1 << 9 if player_input['s5'] else 0) +
-        (1 << 10 if player_input['s6'] else 0) +
-        (1 << 11 if player_input['s7'] else 0) +
-        (1 << 12 if player_input['s8'] else 0) +
-        (1 << 13 if player_input['s9'] else 0) +
-        (1 << 14 if player_input['s10'] else 0) +
-        (1 << 15 if player_input['s11'] else 0) +
-        (1 << 16 if player_input['s12'] else 0))
+        (1 << 0 if input['up'] else 0) +
+        (1 << 1 if input['left'] else 0) +
+        (1 << 2 if input['down'] else 0) +
+        (1 << 3 if input['right'] else 0) +
+        (1 << 4 if input['jump'] else 0) +
+        (1 << 5 if input['use'] else 0) +
+        (1 << 6 if input['s1'] else 0) +
+        (1 << 7 if input['s2'] else 0) +
+        (1 << 8 if input['s3'] else 0) +
+        (1 << 9 if input['s4'] else 0) +
+        (1 << 10 if input['s5'] else 0) +
+        (1 << 11 if input['s6'] else 0) +
+        (1 << 12 if input['s7'] else 0) +
+        (1 << 13 if input['s8'] else 0) +
+        (1 << 14 if input['s9'] else 0) +
+        (1 << 15 if input['s10'] else 0) +
+        (1 << 16 if input['s11'] else 0) +
+        (1 << 17 if input['s12'] else 0))
 
 
-def deserialize_client_input(player_input):
-    pass    
+def deserialize_client_input(input):
+    target = input[3:7].decode()
+    player_input = int(input[7::].decode())
+
+    actions = {
+        'up': player_input & 1,
+        'left': player_input & (1 << 1),
+        'down': player_input & (1 << 2),
+        'right': player_input & (1 << 3),
+        'jump': player_input & (1 << 4),
+        'use': player_input & (1 << 5),
+        's1': player_input & (1 << 6),
+        's2': player_input & (1 << 7),
+        's3': player_input & (1 << 8),
+        's4': player_input & (1 << 9),
+        's5': player_input & (1 << 10),
+        's6': player_input & (1 << 11),
+        's7': player_input & (1 << 12),
+        's8': player_input & (1 << 13),
+        's9': player_input & (1 << 14),
+        's10': player_input & (1 << 15),
+        's11': player_input & (1 << 16),
+        's12': player_input & (1 << 17),
+    }
+
+    return actions, target
 
 
 def serialize_state(state):
