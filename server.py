72fe2e335bdd429f540556b353301f6cfc9458c5
s-brez <sam@sdbgroup.io>
2020-10-24 18:47:57 +1100

    add more message case handling to server

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 
+from messaging import serialize_state, deserialize_client_input
 from player import Player
 from world import World
 
@@ -12,33 +13,42 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((UDP_IP, UDP_PORT))
 
 
-def load_state():
-    return [[Player('0'), Player('1')], World()]
+def load_stored_game_state():  # TODO: don't hard-code this later
+    p0 = Player('0')
+    p1 = Player('1')
+
+    world = World()
+
+    return {p0.ID: p0, p1.ID: p1}, world
 
 
-players, world = load_state()
-slots = {p.ID: None for p in players}
+players, world = load_stored_game_state()
+slots = {p.ID: None for p in players.values()}
+clients = slots.keys()
 
 while True:
 
-    """ First byte of client message dictates what message type it is
-        Second byte is free for use as a boolean response to join requests
-        Third and fourth bytes indicate player ID, 00 - 99. New user IP and
-        port is allocated to a slot, with their player ID sent back in bytes 3
-        and 4. Futher packets from that player must come from their initial IP
-        and port, or they will be rejected."""
-
+    # First byte of client message dictates what message type it is
     data, address = server_socket.recvfrom(BUFFER_SIZE)
 
-    # !st byte is '1', player input
+    # Player input
     if(data[:1] == b'1'):
-        print("Player input received")
+        p_id = data[1:3].decode()
+        if p_id in slots:
+            player_input = deserialize_client_input(data)
 
-    # 1st byte is '2', player chat message
+            # Update player state
+                        
+
+    # Player chat message
     elif (data[:1] == b'2'):
         print("Chat message received")
 
-    # 1st byte is '3', join request
+    # Player join request. Second byte is used as a boolean response
+    # to join requests. Third and fourth bytes indicate player ID, 00 - 99.
+    # New user IP and port is allocated to a slot, with their player ID sent
+    # back in bytes 3 and 4. Futher packets from that player must come from
+    # their initial IP and port, or they will be rejected.
     elif (data[:1] == b'3'):
         p_id = data[1:3].decode()
 
@@ -51,6 +61,8 @@ while True:
                 print("Player", p_id, "joined from", token)
                 slots[p_id] = token
                 server_socket.sendto(("3" + "1" + p_id).encode(), (address[0], address[1]))
+                world.add_player(players[p_id])
+                print(world.players)
 
             # Double connect, reject
             elif slots[p_id] is not None and slots[p_id] != token:
@@ -62,6 +74,23 @@ while True:
             print("Connection attempt by unknown player from", token)
             server_socket.sendto(("3" + "0" + p_id).encode(), (address[0], address[1]))
 
+    # Player exit message
+    elif (data[:1] == b'4'):
+        p_id = data[2:4].decode()
+        print(p_id)
+        if p_id in slots:
+            token = address[0] + ":" + str(address[1])
+            print("Exit message from", token)
+
+            # Clear the players address from player slots
+            pass
+
+    # Error case, message doesnt match expected format
     else:
         print("Unknown message from",
               address[0] + ":" + str(address[1]) + ":", data)
+
+    # Update game state for authenticated clients
+    for p_id in clients:
+        if slots[p_id] is not None:
+            server_socket.sendto(('1' + p_id).encode(), (address[0], address[1]))
