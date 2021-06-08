ef7303335656f7638720a0259370fba79c84041f
s-brez <sam@sdbgroup.io>
2021-06-09 01:20:57 +1000

    add SHA256 impl to shared

--- src/shared/Shared.h
+++ src/shared/Shared.h
@@ -4,6 +4,11 @@
 #define EXIT_SUCCESS                1
 #define EXIT_FAILURE                -1
 
+// Cryptography vars, dont change
+#define CHUNK_SIZE 64
+#define TOTAL_LEN_LEN 8
+#define SALT_SIZE                   26
+
 #define SERVER_DEFAULT_ADDRESS      "127.0.0.1"
 #define SERVER_DEFAULT_PORT         8080
 
@@ -13,6 +18,7 @@
 #define USERNAME_MAX_LENGTH         16
 
 #include <chrono>
+#include <ctime>
 #include <string>
 #include <netdb.h>
 #include <stdio.h>
@@ -26,8 +32,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+
+// Returns ms timestamp as int
 std::string salt();
 
+// Returns SHA256 hash of pwd and salt
 std::string hash(std::string pwd, std::string salt);
+
+// SHA256 implementation
+void calc_sha_256(uint8_t hash[32], const uint8_t *input, size_t len);
+
 
 #endif
